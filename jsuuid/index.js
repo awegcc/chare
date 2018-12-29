8dbc950bb170fff8fc5fc6cc9ddadfa97a870219
awegcc <dawter@qq.com>
2018-12-29 15:11:20 +0800

    Add code

--- jsuuid/index.js
+++ jsuuid/index.js
@@ -1,2 +1,35 @@
-console.log('hello js');
+const si = require('systeminformation');
 
+/*
+si.cpu((info) => {
+    console.log('cpu: ', info);
+});
+*/
+
+/*
+si.bios((info) => {
+    console.log('bios: ', info);
+});
+*/
+
+/*
+si.blockDevices((info) => {
+    console.log('blockDevices: ', info);
+});
+*/
+
+si.baseboard((info) => {
+    console.log('baseboard: ', info);
+});
+
+si.diskLayout((info) => {
+    console.log('diskLayout: ', info);
+});
+
+si.networkInterfaces((info) => {
+    console.log('networkInterfaces: ', info);
+});
+
+si.dockerAll((info) => {
+    console.log('dockerAll: ', info);
+});
